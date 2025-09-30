2bc3800710cf7afe34ceee4b9f5e3d62ce3d667b
oldmartijntje_TH <oldmartijntje@gmail.com>
2025-09-30 10:59:26 +0200

    plain html desing of the app

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware to parse JSON
+app.use(cors());
 app.use(express.json());
 
 // In-memory storage for tasks
